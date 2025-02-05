e371e9621874b73326af608f0e06f73fd46bdb0c
avishkar-kurkute <avishkarkurkute701@gmail.com>
2025-02-05 18:43:28 +0530

    Styling to the Home component

--- projectCotasker/projectCotasker/src/main/java/spring/controller/Controllers.java
+++ projectCotasker/projectCotasker/src/main/java/spring/controller/Controllers.java
@@ -20,39 +20,40 @@ import spring.service.Services;
 @CrossOrigin
 @RequestMapping("/Api/User")
 public class Controllers {
-	
+
 	@Autowired
-	Services service; 
-	
+	Services service;
+
 	@PostMapping("/Register")
-	public ResponseEntity<UserDTOResponse> addUser(@Valid @RequestBody UserDTORequest dto){
-		
+	public ResponseEntity<UserDTOResponse> addUser(@Valid @RequestBody UserDTORequest dto) {
+
 		return ResponseEntity.ok(service.addUser(dto));
 	}
-	
+
 	@PostMapping("/Signin")
 	public boolean signin(@RequestParam String username, @RequestParam String password) {
-	return service.signin(username, password);
+		return service.signin(username, password);
 	}
-	
-	  @PutMapping("/Delete")
-	    public ResponseEntity<String> deleteUser(@RequestParam String username, @RequestParam String password) {
-	        boolean deleted = service.deleteUser(username, password);
-	        
-	        if (deleted) {
-	            return ResponseEntity.ok("User deleted successfully.");
-	        } else {
-	            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found or incorrect password.");
-	        }
-	    }
-	
+
+	@PutMapping("/Delete")
+	public ResponseEntity<String> deleteUser(@RequestParam String username, @RequestParam String password) {
+		boolean deleted = service.deleteUser(username, password);
+
+		if (deleted) {
+			return ResponseEntity.ok("User deleted successfully.");
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found or incorrect password.");
+		}
+	}
+
 	@PutMapping("/UpdatePassword")
-	public ResponseEntity<UserDTOResponse> updatePassword(@RequestParam String username, @RequestParam String password, @RequestParam String newpassword){
+	public ResponseEntity<UserDTOResponse> updatePassword(@RequestParam String username, @RequestParam String password,
+			@RequestParam String newpassword) {
 		return ResponseEntity.ok(service.updatePassword(username, password, newpassword));
 	}
-	
+
 	@PostMapping("/Logout")
-	public ResponseEntity<UserDTOResponse> logout(){
+	public ResponseEntity<UserDTOResponse> logout() {
 		return ResponseEntity.ok(service.logout());
 	}
 }
